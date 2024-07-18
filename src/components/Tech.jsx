8e04361fe6fdd8a37f9227b36141a9f5b6a6cd1e
Robert051998 <96495255+Robert051998@users.noreply.github.com>
2024-07-18 15:23:44 +0200

    reverse some changes

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,33 +4,15 @@ import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
-
 const Tech = () => {
-  const isMobile = window.matchMedia("(max-width: 500px)").matches;
-  const Map = (isMobile ,technologies)=>{
-    if(isMobile){
-      return(
-    technologies.slice(0,4).map((technology)=>(
-    <div className='w-28 h-28' key={technology.name}>
-    <BallCanvas icon={technology.icon}/>
-    <p className="flex justify-center text-white font-bold">{technology.name}</p>
-    </div>
-    )))
-    }
-    else{return(
-      technologies.map((technology)=>(
-        <div className='w-28 h-28' key={technology.name}>
-        <BallCanvas icon={technology.icon}/>
-        </div>
-      )))
-    }
-  }
   return (
-    <>
     <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {Map(isMobile ,technologies)}
+      {technologies.map((technology) => (
+        <div className='w-28 h-28' key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
     </div>
-    </>
   );
 };
 
